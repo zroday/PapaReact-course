f8877f54bd997f1d0a5eb70bc6753d8c0653c28a
zroday <eu.zeroday@gmail.com>
2021-10-21 20:27:36 -0300

    First app files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './Header';
+
+//BEM - Block Element modifier
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+       <h1 className="app__title">This is a title</h1>
+       
+
+       
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//BEM - Block Element Modifier
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1 className="header__title">This is the Header</h1>
+            <h2 className="header__user">Admin is signed in</h2>
+        </div>
+    )
+}
+
+export default Header
